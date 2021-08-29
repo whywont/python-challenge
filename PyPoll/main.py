240be89987f9b1c5c83cbca3b6cd210ea7330d57
whywont <andnotes16@gmail.com>
2021-08-28 23:11:35 -0700

    Update PyPoll to a solution using a dictionary.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,17 +2,11 @@ import csv
 import os
 
 total_votes = 0
-khan_vote = 0
-correy_vote = 0
-li_vote = 0
-o_tooley_vote = 0
-
-
 
 #specifies directory for csv file
 poll_data = os.path.join("Resources", "election_data.csv")
 
-
+pol_dict= {}
 #specifies directory for csv file
 with open(poll_data, 'r', encoding= 'utf-8') as csvfile:
 
@@ -21,36 +15,41 @@ with open(poll_data, 'r', encoding= 'utf-8') as csvfile:
     #ingores header
     header = next(csvReader)
 
+    #iterates through csv file
     for row in csvReader:
+        #counts every row
         total_votes+= 1
-        if row[2] == "Khan":
-            khan_vote+= 1
-        elif row[2] == "Correy":
-            correy_vote+= 1
-        elif row[2] == "Li":
-            li_vote+= 1
+        #Builds dictionary. Adds key name if unique and sets value to 1.
+        #If key value is contained in dictionary, key value is incremented
+        if row[2] not in pol_dict:
+            pol_dict[row[2]] = 1
         else:
-            o_tooley_vote+=1
+            pol_dict[row[2]] += 1
+
     
-pol_dict= {'Kahn': khan_vote, 'Correy': correy_vote, 'Li' :li_vote, "O'Tooley" : o_tooley_vote}
+#gets key of the max value in the dictionary
+winner = max(pol_dict, key=pol_dict.get) if pol_dict else None
 
-sort_pol = sorted(pol_dict.items(), key=lambda x: x[1], reverse=True)
-
-lis_win = [x[0] for x in sort_pol]
-
+#specifies directory for output file
 txt_file = os.path.join("analysis", "poll_data.txt")
 
-#print(lis_win[0])
-
+#Prints values and gets the percentage of votes in one go
 print("Election Results\n-------------------------\nTotal Votes: " + str(total_votes) + "\n-------------------------")
-for i in sort_pol:
-    print(i[0], str(round((int(i[1])/total_votes * 100), 3)) + '%', '(' + str(i[1]) + ')')
-print('-------------------------\n Winner: ' + str(lis_win[0]) +'\n-------------------------')
- 
+for k,v in pol_dict.items():
+    print(k + ' ' + str(round((int(v)/total_votes * 100), 3)) + '% (' + str(v) + ') \n')
+print('-------------------------\nWinner: '  + winner +'\n-------------------------')   
 
-#creates txt file with writer permisions and adds data
+#writes values and gets the percentage of votes to a csv file
 with open(txt_file, 'w', encoding= 'utf-8') as txtfile:
     txtfile.write("Election Results\n-------------------------\nTotal Votes: " + str(total_votes) + "\n-------------------------\n")
-    for i in sort_pol:
-        txtfile.write(str(i[0]) + ' ' + str(round((int(i[1])/total_votes * 100), 3)) + '% (' + str(i[1]) + ') \n')
-    txtfile.write('-------------------------\n Winner: ' + str(lis_win[0]) +'\n-------------------------')
+    for k,v in pol_dict.items():
+        txtfile.write(k + ' ' + str(round((int(v)/total_votes * 100), 3)) + '% (' + str(v) + ') \n')
+    txtfile.write('-------------------------\nWinner: '  + winner +'\n-------------------------')   
+
+            
+        
+    
+#pol_dict= {'Kahn': khan_vote, 'Correy': correy_vote, 'Li' :li_vote, "O'Tooley" : o_tooley_vote}
+
+#sort_pol = sorted(pol_dict.items(), key=lambda x: x[1], reverse=True)
+
